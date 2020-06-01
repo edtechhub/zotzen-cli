e80242fa5995b318eb74f5171b88eeea7acc4887
Abhimanyu Gangwar <ag15081993@gmail.com>
2020-06-01 20:52:37 +0100

    Added json and open flags for item creation

--- zotzen.js
+++ zotzen.js
@@ -1,6 +1,8 @@
 const ArgumentParser = require('argparse').ArgumentParser;
 const childProcess = require('child_process');
 const fs = require('fs');
+const opn = require('opn');
+const path = require('path');
 
 const parser = new ArgumentParser({
   version: '1.0.0',
@@ -10,9 +12,16 @@ const parser = new ArgumentParser({
 
 parser.addArgument('--new', {
   action: 'storeTrue',
-  help: 'Create a new document',
+  help: 'Create a new document.',
 });
-parser.addArgument('--title', { help: 'Create a new document' });
+parser.addArgument('--title', { help: 'Title of the new document.' });
+parser.addArgument('--json', {
+  help: 'Path of the json for the new document.',
+});
+parser.addArgument('--open', {
+  action: 'storeTrue',
+  help: 'Open the zotero and zenodo link after creation.',
+});
 
 const args = parser.parseArgs();
 
@@ -52,39 +61,47 @@ function parseFromZenodoResponse(content, key) {
     .trim();
 }
 
-function zotzenCreate(title) {
-  // Create zotero record
+function zoteroCreate(title, jsonFile = null) {
+  if (jsonFile) {
+    return JSON.parse(
+      runCommand(`create-item ${path.join(__dirname, jsonFile)}`, true)
+    );
+  }
+
   const zoteroCreateItemTemplate = runCommand(
     'create-item --template report',
     true
   );
   const templateJson = JSON.parse(zoteroCreateItemTemplate);
   templateJson.title = title;
-  const newItem = JSON.parse(
+  return JSON.parse(
     runCommandWithJsonFileInput('create-item', templateJson, true)
   );
+}
 
-  // Create zenodo record
+function zenodoCreate(zoteroRecord, zoteroSelectLink) {
   const zenodoTemplate = JSON.parse(
     fs.readFileSync(zenodoCreateRecordTemplatePath).toString()
   );
-  const zoteroSelectLink = newItem.successful[0].links.self.href.replace(
+  zenodoTemplate.related_identifiers[0].identifier = zoteroSelectLink;
+  zenodoTemplate.title = zoteroRecord.successful[0].data.title;
+  zenodoTemplate.description = zoteroRecord.successful[0].data.title;
+  return runCommandWithJsonFileInput('create --show', zenodoTemplate, false);
+}
+
+function zotzenCreate(args) {
+  const zoteroRecord = zoteroCreate(args.title, args.json);
+  const zoteroSelectLink = zoteroRecord.successful[0].links.self.href.replace(
     zoteroApiPrefix,
     zoteroSelectPrefix
   );
-  zenodoTemplate.related_identifiers[0].identifier = zoteroSelectLink;
-  zenodoTemplate.title = title;
-  zenodoTemplate.description = title;
-  const zenodoRecord = runCommandWithJsonFileInput(
-    'create --show',
-    zenodoTemplate,
-    false
-  );
 
+  const zenodoRecord = zenodoCreate(zoteroRecord, zoteroSelectLink);
   const doi = parseFromZenodoResponse(zenodoRecord, 'DOI');
   const zenodoDepositUrl = parseFromZenodoResponse(zenodoRecord, 'URL');
+
   runCommandWithJsonFileInput(
-    `update-item --key ${newItem.successful['0'].key}`,
+    `update-item --key ${zoteroRecord.successful['0'].key}`,
     {
       extra: doi,
     }
@@ -92,19 +109,22 @@ function zotzenCreate(title) {
 
   console.log('Item successfully created: ');
   console.log(
-    `Zotero ID: ${newItem.successful[0].library.id}:${newItem.successful[0].key}`
+    `Zotero ID: ${zoteroRecord.successful[0].library.id}:${zoteroRecord.successful[0].key}`
   );
-  console.log(`Zotero link: ${newItem.successful[0].links.self.href}`);
+  console.log(`Zotero link: ${zoteroRecord.successful[0].links.self.href}`);
   console.log(`Zotero select link: ${zoteroSelectLink}`);
   console.log(
     `Zenodo RecordId: ${parseFromZenodoResponse(zenodoRecord, 'RecordId')}`
   );
   console.log(`Zenodo DOI: ${doi}`);
   console.log(`Zenodo deposit link: ${zenodoDepositUrl}`);
+
+  if (args.open) {
+    opn(zoteroSelectLink);
+    opn(zenodoDepositUrl);
+  }
 }
 
 if (args.new) {
-  if (args.title) {
-    zotzenCreate(args.title);
-  }
+  zotzenCreate(args);
 }
