c5e77dfbf9a3a142eb01f06da7ac07be51029f1c
Björn Haßler <bjohas+git@gmail.com>
2020-06-10 17:45:40 +0100

    Update install file.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-git submodule update --init --recursive
+#git submodule update --init --recursive
 cd zotero-cli
 npm install
-npm run build
+npm build run
 cd ..
 cd zenodo-cli
 pip3 install -r requirements.txt
